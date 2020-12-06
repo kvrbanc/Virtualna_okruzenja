054e4afa56d34dfc69afa5b8ce4fda823067e5ac
Matej Đanić <matej.danic@triple-innovations.com>
2020-12-06 16:46:21 +0100

    configure server submodule

--- back/src/main/java/virokr/repositories/ScoreRepository.java
+++
@@ -1,9 +0,0 @@
-package virokr.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import virokr.models.HighScore;
-
-public interface ScoreRepository extends JpaRepository<HighScore, Integer> {
-	
-}
